31a683eba97d7da37752c984ea3c8e9d94309310
Lewis <matt@csiga.org>
2018-06-04 15:55:16 -0500

    Removed unused function from api.s

--- src/api.js
+++ src/api.js
@@ -1,11 +1,2 @@
 export const ROOT_URL = 'http://localhost:5000'
 export const UNIVERSITIES_URL = ROOT_URL + '/universities'
-
-export function fetchUniversities(){
-    var universities = fetch(
-        UNIVERSITIES_URL
-      ).then(
-        response => response.json()
-      );
-    return universities;
-}
