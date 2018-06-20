f1f381d16f6b7b60e6a6a9151f0d2c14a2f1c029
Lewis <matt@csiga.org>
2018-06-20 15:10:27 -0500

    Stuff

--- smart_scrape.py
+++ smart_scrape.py
@@ -1,23 +1,25 @@
 import csv
-from app.models import db,Course,CoreRequirement,CoreComponent,Program
+from app.models import db,Course,CoreRequirement,CoreComponent,Program,ProgramCoreRequirement
 
-CURRICULUM_FILE = open('./UHCL_program_data.csv','r')
-found_programs = set()
-programs = set()
+CURRICULUM_FILE = open('./UHCL_program_data-csv.csv','r')
+missing_programs = set()
+univ_id = 1
 
-reader = csv.reader(CURRICULUM_FILE)
-for degree_name,rubric_number,course_name,component_area,requirement_area,*_ in reader:
-    programs.add(degree_name)
-    rubric,number = rubric_number.split(" ")
-    course = db.session.query(Course).filter(Course.rubric==rubric,Course.number==number).first()
-    program = db.session.query(Program).filter(Program.name==degree_name).first()
-    if(program):
-        found_programs.add(degree_name)
-        if(course):
-            core_requirement = db.session.query(CoreRequirement).filter(CoreRequirement.course_id == course.id).first()
-            if(core_requirement):
-                core_component = db.session.query(CoreComponent).filter(CoreComponent.id == core_requirement.core_component_id).first()
-                print(f'{rubric} {number} - {course_name} is a {core_component.name} requirement for {program.name}')
-    else:
-        print(f'No program: {degree_name}')
-print(programs.difference(found_programs))
+reader = csv.DictReader(CURRICULUM_FILE)
+
+for dict_ in reader:
+    degree_name = dict_['degree_name']
+    course_rubric = dict_['course_rubric']
+    course_number = dict_['course_number']
+    course_name = dict_['course_name']
+    other_component_name = dict_['other_component_name']
+    other_component_hours = dict_['other_component_hours']
+    course = db.session.query(Course).filter(Course.rubric==course_rubric,Course.number==course_number,Course.univ_id==univ_id).first()
+    program = db.session.query(Program).filter(Program.name==degree_name,Program.univ_id==univ_id).first()
+    if(not program):
+        missing_programs.add(degree_name)
+        continue
+    core_requirement = db.session.query(CoreRequirement).filter(CoreRequirement.course_id==course.id).first()
+    if(core_requirement):
+        
+print(missing_programs)
